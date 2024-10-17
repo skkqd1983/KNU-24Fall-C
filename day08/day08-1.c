f79948719e2884860e09519110f8d8eb879fa313
karick4398 <skkqd1983@gmail.com>
2024-10-17 17:07:10 +0900

    문자열 뒤집기v2

--- day08/day08-1.c
+++ day08/day08-1.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
-#include<string.h>   // strlen()
+#include<string.h>
 #include <stdlib.h>
-
-void Reverse(char* str);
 
 int main() {
 
-    char* rstr = (char*)malloc(sizeof(char) * 100);   // NULL을 포함한 크기(size)를 입력한다.
+    char* rstr = (char*)malloc(sizeof(char) * 100); 
+    char str;
 
     printf("문자열을 입력하세요 : ");
     scanf_s("%s", rstr, 100);
 
     printf("뒤집어진 문자열 : ");
-    for (int i = strlen(rstr) - 1; i >= 0; i--) {   // NULL을 제외한 문자열 길이를 초기값으로 준다.
-        printf("%c", rstr[i]);
+    for (int i = 0; i < strlen(rstr) / 2; i++) {
+        str = rstr[strlen(rstr) - i -1];
+        rstr[strlen(rstr) - i - 1] = rstr[i];
+        rstr[i] = str;
     }
-    printf("\n");
+    printf("%s\n", rstr);
 
     return 0;
 }
