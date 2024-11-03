c9d65371b5c62461f352464f0c5d6339a609cec1
karick4398 <skkqd1983@gmail.com>
2024-11-03 15:39:27 +0900

    메뉴, 난이도 기능
    메뉴에서 난이도를 선택해
    Easy(1) Normal(2) Hard(3)에 따라
    Sleep(200/difficulty_level);하여
    난이도가 높으면 뱀의 속도가 증가합니다.

--- snake_game/source.c
+++ snake_game/source.c
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <conio.h> // 콘솔 입출력 함수 제공
 
+
+#pragma region Define 정의들
+
 #define COUT GetStdHandle(STD_OUTPUT_HANDLE) // 표준 출력 디바이스
 #define Map_X 40 // 맵 X크기
 #define Map_Y 20 // 맵 Y크기
@@ -13,11 +16,13 @@
 #define RED 4
 #define BLUE 1
 
+/* 뱀의 이동 방향 */
 #define Left 1
 #define Right 2
 #define Up 3
 #define Down 4
 
+/* 맵의 구성요소 */
 #define Empty 0
 #define Ceilling 1
 #define Wall 2
@@ -25,6 +30,24 @@
 #define Snake_Head -1
 #define Snake_Body -2
 
+/* 게임 모드 */
+#define Solo_mod 1
+#define Battle_mod 2
+
+/* 게임 난이도 */
+#define Easy 1
+#define Normal 2
+#define Hard 3
+
+/* 현재 선택한 목록 */
+#define Game_mod 1
+#define Difficulty_level 2
+
+#pragma endregion
+
+#pragma region 각종 변수
+
+int best_score = 0;
 int score = 0;															// 게임 스코어 관련 변수
 int gameFlag = 0;														// 게임 시작 여부 관련 변수
 int gameEnd = 0;														// 게임 종료 여부 변수
@@ -35,7 +58,13 @@ int mapOrigin[Map_Y][Map_X];											// 실질적 맵
 int mapCopy[Map_Y][Map_X];												// 맵의 복사본(변경 전 행태를 가짐)
 int foodX = 0, foodY = 0;												// 음식의 위치 변수
 
-int RW = 0, LW = 0;
+int RW = 0, LW = 0; // 테스트용 변수
+
+int game_mod = Solo_mod;												// 게임 모드
+int difficulty_level = Easy;											// 게임의 난이도
+int select_menu = Game_mod;													// 메뉴에서 선택중인 항목
+
+#pragma endregion
 
 void map_reset();														// 최초 시작시 맵 기본 설정 함수
 void draw_map();														// 맵의 요소가 변경될 시 반영하는 함수	
@@ -43,6 +72,12 @@ void input_game();														// 키보드의 입력으로 뱀의 방향을 부여하는 함수
 void snake_move();														// 뱀의 움직임을 제어하는 함수
 void add_body();														// 뱀의 몸을 늘리는 함수
 void food_spawn();														// 음식 섭취 시 재소환하는 함수
+void print_game_menu();													// 게임 메뉴 출력
+void gameStartScreen();
+void gamePlayingScreen();
+void inputGameMenu();
+
+#pragma region 노드 관련 구조체, 몸통 길이 조절
 
 struct NODE* head;
 
@@ -98,6 +133,9 @@ int delete_node() {														// 노드 전부 삭제
 	return 0;
 }
 
+#pragma endregion
+
+#pragma region 콘솔 커서 숨기는 함수
 
 typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCURSOR } CURSOR_TYPE;		// 커서숨기는 함수에 사용되는 열거형 
 
@@ -121,6 +159,8 @@ void setcursortype(CURSOR_TYPE c) {										// 커서숨기는 함수
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CurInfo);
 }
 
+#pragma endregion
+
 void mouseXY(int x, int y) {											// 콘솔 마우스 위치 조정 함수 
 	COORD pos;
 	pos.X = x;
@@ -134,18 +174,135 @@ void main() {
 
 	srand((unsigned)time(NULL));
 	setcursortype(NOCURSOR);											// 커서 없앰 
+	while (1) {
+		if (gameFlag == 0) {
+			gameEnd = 0;
+			gameStartScreen();
+		}
+		else {
+			gamePlayingScreen();
+		}
+	}
+}
+
+void gameStartScreen() {
+	print_game_menu();
+
+	while (gameFlag != 1) {
+		inputGameMenu();
+	}
+}
+
+void gamePlayingScreen() {
+	system("cls");
+	score = 0;
+
 	map_reset();
 	food_spawn();
 	draw_map();
-	while (1) {
+	while (gameFlag == 1) {
 		input_game();
 		snake_move();
 	}
 }
 
+void print_game_menu() {
+	system("cls");
+	printf("[현재 스코어]\n");
+	printf("Best Score : %d | Last Score : %d\n\n", best_score, score);
+
+	printf("[게임 메뉴]\n");
+	printf("▶솔로 모드\n");
+	printf("▷Easy");
+}
+
+void inputGameMenu() {
+	char key = getch(); // getch()는 키보드의 하나 키를 입력 받을 수 있게 하는 함수
+
+	if (key == 'w' || key == 'W') {
+		if (select_menu == Difficulty_level) {
+			select_menu = Game_mod;
+			mouseXY(0, 4);
+			printf("▶\n\r▷");
+		}
+	}
+	else if (key == 's' || key == 'S') {
+		if (select_menu == Game_mod) {
+			select_menu = Difficulty_level;
+			mouseXY(0, 4);
+			printf("▷\n\r▶");
+		}
+	}
+	else if (key == 'd' || key == 'D') {
+		if (select_menu == Game_mod) {
+			mouseXY(1, 4);
+			if (game_mod == Solo_mod) {
+				game_mod = Battle_mod;
+				printf("배틀 모드");
+			}
+			else if (game_mod == Battle_mod) {
+				game_mod = Solo_mod;
+				printf("솔로 모드");
+			}
+		}
+		else if (select_menu == Difficulty_level) {
+			mouseXY(1, 5);
+			if (difficulty_level == Easy) {
+				difficulty_level = Normal;
+				printf("Normal");
+			}
+			else if (difficulty_level == Normal) {
+				difficulty_level = Hard;
+				printf("Hard  ");
+			}
+			else if (difficulty_level == Hard) {
+				difficulty_level = Easy;
+				printf("Easy  ");
+			}
+		}
+	}
+	else if (key == 'a' || key == 'A') {
+		if (select_menu == Game_mod) {
+			mouseXY(1, 4);
+			if (game_mod == Solo_mod) {
+				game_mod = Battle_mod;
+				printf("배틀 모드");
+			}
+			else if (game_mod == Battle_mod) {
+				game_mod = Solo_mod;
+				printf("솔로 모드");
+			}
+		}
+		else if (select_menu == Difficulty_level) {
+			mouseXY(1, 5);
+			if (difficulty_level == Easy) {
+				difficulty_level = Hard;
+				printf("Hard  ");
+			}
+			else if (difficulty_level == Normal) {
+				difficulty_level = Easy;
+				printf("Easy  ");
+			}
+			else if (difficulty_level == Hard) {
+				difficulty_level = Normal;
+				printf("Normal");
+			}
+		}
+	}
+	else if (key == '\r') {
+		switch (select_menu) {
+		case Game_mod:
+			gameFlag = 1;
+			break;
+		case Difficulty_level:
+			exit(0); // 프로그램을 종료 하는 함수
+			break;
+		}
+	}
+}
+
 void map_reset() {
 	int i, j;
-
 
 	for (i = 0; i < 3; i++) {
 		insert_node_last(create_node(20 + i, 10, Left));
@@ -184,7 +341,7 @@ void map_reset() {
 	}
 }
 
-void draw_map() {
+void draw_map() {	// 이전 맵과 비교하여 다른 부분을 작성
 	int i, j;
 	for (i = 0; i < Map_X; i++) {
 		for (j = 0; j < Map_Y; j++) {
@@ -222,7 +379,7 @@ void draw_map() {
 			mapCopy[j][i] = mapOrigin[j][i];
 		}
 	}
-}
+} 
 
 void input_game() {
 
@@ -250,7 +407,7 @@ void input_game() {
 void snake_move() {
 	int i;
 
-	Sleep(200); // 딜레이
+	Sleep(200/difficulty_level); // 딜레이
 
 	struct NODE* cur = head->next_link;
 	snake_move_point(); //뱀 이동 방향 이전
